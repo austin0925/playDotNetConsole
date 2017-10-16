03ae15bfd78b55f7cbd78ecc64865c2c548c6d2e
austin-wang <austin-wang@nec.com.tw>
2017-10-16 16:37:11 +0800

    check console is correct

---
+++ ScriptTest/ScriptTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScriptTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = Console.ReadKey();
+            Console.WriteLine("asdf");
+        }
+    }
+}
