6bd27c5455736c67418fc1651cfe86ed10ada45f
austin-wang <austin-wang@nec.com.tw>
2017-10-17 08:39:40 +0800

    bcup

--- ScriptTest/ScriptTest/Program.cs
+++ ScriptTest/ScriptTest/Program.cs
@@ -6,6 +6,11 @@ namespace ScriptTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("abc" ?? "abc");
+            Console.WriteLine("abc" ?? "abC");
+            Console.WriteLine("abC" ?? "abc");
+            Console.WriteLine("abC" ?? "abC");
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
